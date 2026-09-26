8fb5a2d351c9aac59ec8f87ad682bc0c72f48d30
emdstafford <emdstafford@gmail.com>
2026-09-26 14:36:39 -0400

    Fix client drag event type import

--- components/UspsRateImport.tsx
+++ components/UspsRateImport.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { DragEvent, useMemo, useRef, useState } from "react";
+import { useMemo, useRef, useState } from "react";\nimport type { DragEvent } from "react";
 import * as XLSX from "xlsx";
 import { supabase } from "@/lib/supabase";
 
